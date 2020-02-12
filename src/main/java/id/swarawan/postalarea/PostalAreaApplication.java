0ea5c7eb7eeaab054a1e83f64a7c48b8a6b7edcb
Rio Swarawan Putra <swarawan.rio@gmail.com>
2020-02-13 05:46:15 +0700

    [RIO] add spring factory meta

--- src/main/java/id/swarawan/postalarea/PostalAreaApplication.java
+++ src/main/java/id/swarawan/postalarea/PostalAreaApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan("id.investree.core")
 public class PostalAreaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PostalAreaApplication.class, args);
-	}
 
 }
